25e9ccffa7f69d2aae5154c94387f309c13192e0
sparklinstar <souradip1000@gmail.com>
2024-02-04 14:24:16 +0530

    changes made

--- mainV2.py
+++ mainV2.py
@@ -46,29 +46,69 @@ data = {
     "prompt": "doctor avatar with a stethoscope, and a white coat, white background, minimalistic",
 }
 
+
 response = requests.post(url, headers=headers, json=data)
 if response.status_code == 200:
-    with open("output_image.png", "wb") as f:
+    with open("doctor.png", "wb") as f:
         f.write(response.content)
 else:
     print(f"Error: {response.status_code}, {response.text}")
-col1, col2 = st.columns(2)
+
 # Load the image from a file or any source
-with col1:
-    image_path = "output_image.png"  # Replace with the path to your image file
-    image = Image.open(image_path)
+
+  
+
+url = "https://api.cloudflare.com/client/v4/accounts/78f84e7a44c04581f6beb072d05136d1/ai/run/@cf/stabilityai/stable-diffusion-xl-base-1.0"
+headers = {
+    "Authorization": "Bearer nrh7rmLsTp0PXwggnTZR2oyP_MZuULPKFhetPyDo",
+    "Content-Type": "application/json",
+}
+
+data = {
+    "prompt": "user avatar with a face, white background, minimalistic",
+}
+
+
+response = requests.post(url, headers=headers, json=data)
+if response.status_code == 200:
+    with open("user.png", "wb") as f:
+        f.write(response.content)
+else:
+    print(f"Error: {response.status_code}, {response.text}")
+
+# Load the image from a file or any source
+
+
 
     # Display the image in the Streamlit app
    
     # Title and Description
-    st.title("UroCare")
-    st.write("Your friendly Urologist Doctor. Ask me any question related to sex health and I will provide you with the best possible answer. I am here to help you.")
+st.title("UroCare")
+st.write("Your friendly Urologist Doctor. Ask me any question related to sex health and I will provide you with the best possible answer. I am here to help you.")
 
-with col2:
-    st.image(image,  width=50, use_column_width=False)
-    # User input
-    input_text = st.text_input('Enter message ')
 
+    
+   
+
+
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    st.chat_message("user",avatar="user.png").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    
     API_BASE_URL = "https://api.cloudflare.com/client/v4/accounts/78f84e7a44c04581f6beb072d05136d1/ai/run/"
     headers = {"Authorization": "Bearer SwP60DGlLcaOHiPHV3fhgUUZ0huUeKAXlbHoQthQ"}
 
@@ -79,23 +119,19 @@ with col2:
 
     inputs = [
         { "role": "system", "content": "You are a Urologist Doctor with the name Dr. Pal who provides friendly assistance and helps to diagnose patients. Dont say you are an ai. Strictly Provide answers only if the question is in the field of Urology " },
-        { "role": "user", "content": input_text}
+        { "role": "user", "content": prompt}
     ];
-    output = run("@cf/meta/llama-2-7b-chat-int8", inputs)
-    print(output)
+    response = run("@cf/meta/llama-2-7b-chat-int8", inputs)
+    print(response)
+    
+    
+    # Display assistant response in chat message container
+    with st.chat_message("assistant",avatar="doctor.png"):
+        st.markdown(response['result']['response'])
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response['result']['response']})
+    
 
-    if st.button('Send Message') and input_text:
-        with st.spinner('Generating response...'):
-            try:
-                # Generate satirical response
-                response = output
-                st.write(response['result']['response'])
-                # Store conversation
-                # st.session_state.past.append(input_text)
-                # st.session_state.generated.append(response)
+    
 
-                #Display conversation in reverse order
-            
-
-            except Exception as e:
-                st.error(f"An error occurred: {str(e)}")
+    
